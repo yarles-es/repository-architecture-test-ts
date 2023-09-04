86a623ffb7598aceb67865c867e564963fde114b
Yarles de Andrade Espirito Santo <yarles@pop-os.localdomain>
2023-09-03 22:04:40 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("oi")
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
